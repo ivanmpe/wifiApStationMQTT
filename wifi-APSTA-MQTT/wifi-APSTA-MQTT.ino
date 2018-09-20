4fd7bb9c0e577f51a77bae86a11810f2450c8860
Ivan Sousa <ivanmpe@users.noreply.github.com>
2018-09-20 19:20:21 -0300

    update code

--- wifi-APSTA-MQTT/wifi-APSTA-MQTT.ino
+++ wifi-APSTA-MQTT/wifi-APSTA-MQTT.ino
@@ -19,10 +19,10 @@ const char* senha = ""; // Senha da rede WI-FI que deseja se conectar
 
 // MQTT
 //const char* BROKER_MQTT = "ifce.sanusb.org"; //URL do broker MQTT que se deseja utilizar
-const char* BROKER_MQTT = "m13.cloudmqtt.com";
-int BROKER_PORT = 13728; // Porta do Broker MQTT
-const char* MQTTUSER= "xmvxeajy";
-const char* MQTTPASSWORD = "_rdhkvlq9-aB";
+const char* BROKER_MQTT = "server.cloudmqtt.com";
+int BROKER_PORT = port; // Porta do Broker MQTT
+const char* MQTTUSER= " ";
+const char* MQTTPASSWORD = " ";
 
 //Variáveis e objetos globais
 WiFiClient espClient; // Cria o objeto espClient
